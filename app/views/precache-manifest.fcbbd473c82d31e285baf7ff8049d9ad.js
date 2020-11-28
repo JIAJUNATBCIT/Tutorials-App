bab9a4daddbcf46f4585287116b250a7974b986b
jiajun cai <jiajuncai7@gmail.com>
2020-11-27 17:23:33 -0800

    init commit

---
+++ app/views/precache-manifest.fcbbd473c82d31e285baf7ff8049d9ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6ac0b0fa543b596a744eeba39226dd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f62869b4704533740761",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "7da714ee3e1c1c6e5b62",
+    "url": "/static/css/main.9c6cdb86.chunk.css"
+  },
+  {
+    "revision": "f62869b4704533740761",
+    "url": "/static/js/2.35457f30.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/static/js/2.35457f30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7da714ee3e1c1c6e5b62",
+    "url": "/static/js/main.390a6813.chunk.js"
+  },
+  {
+    "revision": "07858fea14fd66493418",
+    "url": "/static/js/runtime-main.efee8004.js"
+  }
+]);
